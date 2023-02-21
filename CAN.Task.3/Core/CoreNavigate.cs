72a9bf63be7a9b8eb72210eeec7f53f8e974e298
Пользователь <@DESKTOP-KVSP5S9>
2023-02-21 09:50:43 +0300

    Добавьте файлы проекта.

---
+++ CAN.Task.3/Core/CoreNavigate.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace CAN.Task._3.Core
+{
+    public static class CoreNavigate
+    {
+        public static Frame? MyConnection { get; set; }
+    }
+}
---
+++ CAN.Task.3/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CAN.Task._3.Core;
+using CAN.Task._3.View.Pages;
+using System.Windows;
+
+namespace CAN.Task._3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+
+        }
+    }
+}
